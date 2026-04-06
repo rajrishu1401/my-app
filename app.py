cf693d2ce1e315fe073634fd3383cb8cd100fb59
rajrishu1401 <rajrishu1401@gmail.com>
2026-04-06 23:54:59 +0530

    testing webhook

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from CI/CD Pipeline! Rishu is here!"
+    return "Hello from CI/CD Pipeline! Rishu is here!\n Change: Checking if the pipeline is working fine."
     #return "Hello from CI/CD Pipeline!, my sapid is 123456"
 
 app.run(host="0.0.0.0", port=80)
